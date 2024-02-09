b2819365d7c4af5620b53f5876a25aa035ad93af
GyuTaeKim <gyutae100@gmail.com>
2024-02-09 20:55:19 +0900

    refactor(addPositionPropToEachNode): 불필요한 주석 제거 (참고로 커링 시 파라미터 기본값 설정 불가함)

--- src/operators/addPositionPropToEachNode.ts
+++ src/operators/addPositionPropToEachNode.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { map } from "./map";
 
 import { type TOperatorSettings } from "../_internal/type";
@@ -6,12 +5,11 @@ import { Store } from "../_internal/store";
 
 const R = require("ramda");
 
-// @TODO: 자동 값 설정이 제대로 동작 안함. 예: positionKey: string = 'position'
 export const addPositionPropToEachNode = R.curry(
   <TNode>(
     ops: TOperatorSettings,
-    positionKey: string,
-    nodes: TNode[],
+    positionKey: string = "position",
+    nodes: TNode[]
   ): TNode[] => {
     const store = Store({
       position: [0],
@@ -50,7 +48,7 @@ export const addPositionPropToEachNode = R.curry(
     return map(
       defaultOps,
       (el) => R.assoc(positionKey, store.get().position.join("."))(el),
-      nodes,
+      nodes
     );
-  },
+  }
 );
