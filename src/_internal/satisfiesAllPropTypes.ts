decd387cf7f9992665e581e2a807bd650b95e042
GyuTaeKim <gyutae100@gmail.com>
2024-02-09 20:52:38 +0900

    refactor: 불필요한 주석 제거

--- src/_internal/satisfiesAllPropTypes.ts
+++ src/_internal/satisfiesAllPropTypes.ts
@@ -1,12 +1,11 @@
 const R = require("ramda");
 
-// @TODO: internal 폴더로 분리
 export const satisfiesAllPropTypes = R.curry((propsWithTypes, obj) =>
   R.equals(
     R.toPairs(propsWithTypes),
     R.pipe(
       R.toPairs,
-      R.map(([key, value]) => [key, R.type(value)]),
-    )(obj),
-  ),
+      R.map(([key, value]) => [key, R.type(value)])
+    )(obj)
+  )
 );
