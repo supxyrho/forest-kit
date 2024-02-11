5b42b739bf776a9b242cf4f30a91a5bd7b30185d
GyuTaeKim <gyutae100@gmail.com>
2024-02-11 20:51:49 +0900

    docs(verifyNodeAllPropsTypeMatch): Add TODO comments for enhancing type specificity of propTypePairs

--- src/operators/verifyNodeAllPropsTypeMatch.ts
+++ src/operators/verifyNodeAllPropsTypeMatch.ts
@@ -5,10 +5,11 @@ const R = require("ramda");
 
 export const verifyNodeAllPropsTypeMatch = R.curry(
   <TNode>(
-    // @TODO: type 적용
+    // @TODO: 현재 사용 예시는, { key: type } 형태의 object를 받아야 하지만, 현재 any로 처리되고 있음
+    // @TODO: 보다 직관적으로 [ [key, type], [key, type] ] 형태로 변경
     propTypePairs: any,
-    node: TNode,
+    node: TNode
   ): boolean => {
     return satisfiesAllPropTypes(propTypePairs, node);
-  },
+  }
 );
