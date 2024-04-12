91d1225368d36e44c9d5d37e644ea0c502d24b2b
GyuTaeKim <gyutae100@gmail.com>
2024-04-13 00:28:20 +0900

    refactor(addTreeNodePathPropToEachNode): naming refactoring

--- src/operators/addTreeNodePathPropToEachNode.ts
+++ src/operators/addTreeNodePathPropToEachNode.ts
@@ -1,11 +1,10 @@
-import { map } from "./map";
+const R = require("ramda");
 
 import { type TOperatorSettings } from "../_internal/type";
 import { Store } from "../_internal/store";
-
 import { MOVE_DOWN, MOVE_NEXT, MOVE_UP } from "../_internal/constants";
 
-const R = require("ramda");
+import { map } from "./map";
 
 export const addTreeNodePathPropToEachNode = R.curry(
   <TNode>(
@@ -16,35 +15,50 @@ export const addTreeNodePathPropToEachNode = R.curry(
     nodes: TNode[]
   ): TNode[] => {
     const store = Store({
-        currentTreePath: [],
+      currentTreeNodePath: [],
     });
 
     return map(
-        {
+      {
         ...ops,
         onMoveCursor: handleCursorMovement(store, nameKey),
-        },
-        (el) => R.assoc(treeNodePathKey, store.get().currentTreePath.join(joinSeparator))(el),
-        nodes
+      },
+      assocTreeNodePathProp(treeNodePathKey, joinSeparator, store),
+      nodes
     );
-    }
+  }
 );
 
-const moveNext = R.curry((currentPath, nameKey, treeNode) => R.init(currentPath).concat(R.prop(nameKey, treeNode)))
+const assocTreeNodePathProp = R.curry(
+  (treeNodePathKey, joinSeparator, store, treeNode) =>
+    R.assoc(
+      treeNodePathKey,
+      R.join(joinSeparator, store.get().currentTreeNodePath),
+      treeNode
+    )
+);
 
-const moveDown = R.curry((currentPath, nameKey, treeNode)=> R.append(R.prop(nameKey, treeNode), currentPath))
+const moveNext = R.curry((currentPath, nameKey, treeNode) =>
+  R.init(currentPath).concat(R.prop(nameKey, treeNode))
+);
 
-const moveUp = R.curry((currentPath) => R.init(currentPath))
+const moveDown = R.curry((currentPath, nameKey, treeNode) =>
+  R.append(R.prop(nameKey, treeNode), currentPath)
+);
+
+const moveUp = R.curry((currentPath) => R.init(currentPath));
 
 const updatePathByDirection = R.cond([
   [R.equals(MOVE_NEXT), () => moveNext],
   [R.equals(MOVE_DOWN), () => moveDown],
   [R.equals(MOVE_UP), () => moveUp],
-  // @TODO: 에러 throw
-  [R.T, R.identity],
 ]);
 
-const handleCursorMovement = (store, nameKey) => (direction, treeNode) => 
-  store.update({ 
-    currentTreePath: updatePathByDirection(direction)(store.get().currentTreePath, nameKey, treeNode)
+const handleCursorMovement = (store, nameKey) => (direction, treeNode) =>
+  store.update({
+    currentTreeNodePath: updatePathByDirection(direction)(
+      store.get().currentTreeNodePath,
+      nameKey,
+      treeNode
+    ),
   });
--- src/test/addTreeNodePathPropToEachNode.spec.ts
+++ src/test/addTreeNodePathPropToEachNode.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from "@jest/globals";
 import { addTreeNodePathPropToEachNode } from "../operators/addTreeNodePathPropToEachNode";
 
 describe("addTreeNodePathPropToEachNode", () => {
-  test("각각의 노드에 대해서, 트리 경로를 부여한다.", () => {
+  test("각각의 노드를 대상으로, 트리 경로를 부여한다.", () => {
     const originalNodes = [
       {
         name: "1",
@@ -58,7 +58,11 @@ describe("addTreeNodePathPropToEachNode", () => {
                     treePath: "1/1-1/1-1-1/1-1-1-1",
                     children: [],
                   },
-                  { name: "1-1-1-2", treePath: "1/1-1/1-1-1/1-1-1-2", children: [] },
+                  {
+                    name: "1-1-1-2",
+                    treePath: "1/1-1/1-1-1/1-1-1-2",
+                    children: [],
+                  },
                 ],
               },
             ],
@@ -85,8 +89,8 @@ describe("addTreeNodePathPropToEachNode", () => {
     ];
 
     const ops = { childrenKey: "children" };
-    expect(addTreeNodePathPropToEachNode(ops, "treePath", "name", "/", originalNodes)).toEqual(
-      expectedNodes,
-    );
+    expect(
+      addTreeNodePathPropToEachNode(ops, "treePath", "name", "/", originalNodes)
+    ).toEqual(expectedNodes);
   });
 });
