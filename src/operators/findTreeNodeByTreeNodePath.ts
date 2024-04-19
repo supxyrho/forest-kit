1f141168c235fb9911e20307ce19206814e2a4c8
GyuTae Kim <supxyrho@gmail.com>
2024-04-19 15:38:56 +0900

    feat(operators/findTreeNodeByTreeNodePath): TODO 주석 추가

--- src/operators/findTreeNodeByTreeNodePath.ts
+++ src/operators/findTreeNodeByTreeNodePath.ts
@@ -14,6 +14,7 @@ export const findTreeNodeByTreeNodePath = R.curry(
     nodes: TNode[]
   ): TNode[] => 
   pipe(
+    // @TODO: TreeNodePath라는 고정적으로 이름을 사용하지말고, 유일 식별자로서 생성하도록 만든다. 해당 함수 호출 전 treeNode 중 treeNodePath라는 prop이 존재할 수도 있다. react-table 같은 라이브러리 내에서는 어떻게 사용하는지 리서치해본다.
     addTreeNodePathPropToEachNode(ops, 'treeNodePath', treeNodeNameKey, '/'),
     findOneBy(
       ops, 
