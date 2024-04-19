e7e7994be354b259c6ad49335be42130aa31fb7f
GyuTaeKim <gyutae100@gmail.com>
2024-04-19 20:44:30 +0900

    fix: 잘못 기입된 testName 수정

--- src/test/findManyByStartsWithTreeNodePath.spec.ts
+++ src/test/findManyByStartsWithTreeNodePath.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from "@jest/globals";
 import { findManyByStartsWithTreeNodePath } from "../operators/findManyByStartsWithTreeNodePath";
 
 describe("findManyByStartsWithTreeNodePath", () => {
-  test("각각의 노드를 대상으로, 트리 경로를 부여한다.", () => {
+  test("treeNodePath를 포함하는 모든 트리 노드를 반환한다.", () => {
     const originalNodes = [
       {
         name: "1",
@@ -40,16 +40,16 @@ describe("findManyByStartsWithTreeNodePath", () => {
     ];
 
     const expectedNodes = [
-          {
-            name: "2-1",
-            treeNodePath: "2/2-1",
-            children: [
-              { name: "2-1-1", treeNodePath: "2/2-1/2-1-1", children: [] },
-              { name: "2-1-2", treeNodePath: "2/2-1/2-1-2", children: [] },
-            ],
-          },
+      {
+        name: "2-1",
+        treeNodePath: "2/2-1",
+        children: [
           { name: "2-1-1", treeNodePath: "2/2-1/2-1-1", children: [] },
           { name: "2-1-2", treeNodePath: "2/2-1/2-1-2", children: [] },
+        ],
+      },
+      { name: "2-1-1", treeNodePath: "2/2-1/2-1-1", children: [] },
+      { name: "2-1-2", treeNodePath: "2/2-1/2-1-2", children: [] },
     ];
 
     const ops = { childrenKey: "children" };
